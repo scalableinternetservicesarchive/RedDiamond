8836b17fd206855ada367f6fabea0c3c43999a63
Bryan Terce <bryan.terce@gmail.com>
2021-02-15 16:23:50 -0800

    Create edit page for user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def edit
+    @user = User.find_or_create_by(username: params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    @user.update(user_params)
+
+    redirect_to edit_user_path(id: @user.username)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:bio)
+  end
 end
