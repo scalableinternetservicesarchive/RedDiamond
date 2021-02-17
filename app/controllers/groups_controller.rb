556f596a6629498cd7209f5c7e195f71d0d6abfa
boning <boning@ucsb.edu>
2021-02-17 00:13:17 -0800

    Added group post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,8 +3,12 @@ class GroupsController < ApplicationController
         @groups = Group.all
     end
 
-    # def new
-    # end
+    def create 
+        name = params[:group_name]
+        max_count = params[:max_member_count]
+        # TODO: may need to change
+        @group = Group.find_or_create_by(group_name: name, max_member_count: max_count)
+    end
 
     def show #id
         # redirect_to edit_user_path(id: params[:username])
