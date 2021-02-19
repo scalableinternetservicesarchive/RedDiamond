d9324e7b8e654c23f8717ad5aed10bc18481b8f1
shenmo3 <ahliu@umail.ucsb.edu>
2021-02-19 01:16:32 -0800

    add group edit & delete, better view

--- db/migrate/20210217064347_create_groups.rb
+++ db/migrate/20210217064347_create_groups.rb
@@ -3,7 +3,7 @@ class CreateGroups < ActiveRecord::Migration[6.1]
     create_table :groups do |t|
       t.string :group_name
       t.string :activity
-      t.text :group_bio
+      t.text :description
       t.string :leader_name
       t.string :game_name
       t.integer :max_member_count
