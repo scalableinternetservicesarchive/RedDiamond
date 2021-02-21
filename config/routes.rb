7c0aa725555fb8c5c61ace61539c6e46d06795d2
XuandongZhao <3150104910@zju.edu.cn>
2021-02-20 19:42:32 -0800

    add games

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :games
   devise_for :users
   resources :groups
   root 'home#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_21_002124) do
+ActiveRecord::Schema.define(version: 2021_02_21_033927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "group_name"
