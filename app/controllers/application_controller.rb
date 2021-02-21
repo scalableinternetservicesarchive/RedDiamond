0dab271af63f94a2a02e5af7df347ba2b3c93433
Bryan Terce <bryan.terce@gmail.com>
2021-02-20 16:32:13 -0800

    Customize devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_devise, if: :devise_controller?
+
+  protected
+
+  def configure_devise
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
