550c52545eff061d84d7c68c8851a6c607f1f8d1
Bryan Terce <bryan.terce@gmail.com>
2021-02-26 01:06:59 -0800

    Add comments to groups

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,26 @@
-class CommentsController < ApplicationController 
-    def create
-        authenticate_user!
+class CommentsController < ApplicationController
+  def create
+    authenticate_user!
 
-        @user = User.find_by_username(params[:profile_username])
-        @comment = @user.comments.create(comment_params)
-        redirect_to profile_path(@user)
+    if params[:profile_username].present?
+      user = User.find_by_username(params[:profile_username])
+      target = user.comments
+      redirect_target = profile_url(user)
+    elsif params[:group_id].present?
+      group = Group.find(params[:group_id])
+      target = group.comments
+      redirect_target = game_group_url(group.game_id, group)
+    else
+      return head :unprocessable_entity
     end
-    
-    private
-        def comment_params
-            params.require(:comment).permit(:body).merge(commenter: current_user)
-        end
+
+    target.create(comment_params)
+
+    redirect_to redirect_target
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body).merge(commenter: current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   resources :games do
     resources :groups do
       post 'join', on: :member
+      resources :comments, only: %i[create]
     end
   end
   post 'select_game', to: 'home#select_game', as: 'select_game'
   devise_for :users
   root 'home#index'
   resources :profiles, path: '/profile', param: :username do
-    resources :comments
+    resources :comments, only: %i[create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
