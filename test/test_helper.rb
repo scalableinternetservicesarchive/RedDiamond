2dee183cb6883a8857abaae3c0ecaef6139be424
Bryan Terce <bryan.terce@gmail.com>
2021-02-26 10:20:35 -0800

    Simplify form and fix tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require "rails/test_help"
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
+  include Devise::Test::IntegrationHelpers
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
