382a4bdba2e20dcca764f5784c4158a2f5598775
Bryan Terce <bryan.terce@gmail.com>
2021-02-26 13:04:53 -0800

    Properly destroy dependent models

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,9 +2,9 @@ class Group < ApplicationRecord
   validates :max_member_count, numericality: { greater_than_or_equal_to: 2 }
 
   belongs_to :game
-  has_many :group_memberships
+  has_many :group_memberships, dependent: :destroy
   has_many :users, through: :group_memberships
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
 
   alias_attribute :members, :users
 end
